1844413a96d58eb78c80ff09eaf2c6ce9061be07
harupy <hkawamura0130@gmail.com>
2018-09-02 22:20:04 +0900

    update

--- snipping_tool.py
+++ snipping_tool.py
@@ -1,60 +1,58 @@
 import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
-import tkinter as tk
+from PIL import ImageGrab
 
 
 class SnippingTool(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        root = tk.Tk()
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        self.setGeometry(0, 0, screen_width, screen_height)
-        self.setWindowTitle(' ')
-        self.begin = QtCore.QPoint()
-        self.end = QtCore.QPoint()
-        self.setWindowOpacity(0.3)
-        QtWidgets.QApplication.setOverrideCursor(
-            QtGui.QCursor(QtCore.Qt.CrossCursor)
-        )
-        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-        print('Capture the screen...')
-        self.show()
+	def __init__(self):
+		super().__init__()
+		screen_width, screen_height = ImageGrab.grab().size
+		self.setGeometry(0, 0, screen_width, screen_height)
+		self.setWindowTitle(' ')
+		self.begin = QtCore.QPoint()
+		self.end = QtCore.QPoint()
+		self.setWindowOpacity(0.3)
+		QtWidgets.QApplication.setOverrideCursor(
+			QtGui.QCursor(QtCore.Qt.CrossCursor)
+		)
+		self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+		print('Capture the screen...')
+		self.show()
 
-    def paintEvent(self, event):
-        qp = QtGui.QPainter(self)
-        qp.setPen(QtGui.QPen(QtGui.QColor('black'), 2))
-        qp.setBrush(QtGui.QColor(128, 128, 255, 128))
-        qp.drawRect(QtCore.QRect(self.begin, self.end))
+	def paintEvent(self, event):
+		qp = QtGui.QPainter(self)
+		qp.setPen(QtGui.QPen(QtGui.QColor('black'), 2))
+		qp.setBrush(QtGui.QColor(128, 128, 255, 128))
+		qp.drawRect(QtCore.QRect(self.begin, self.end))
 
-    def mousePressEvent(self, event):
-        self.begin = event.pos()
-        self.end = self.begin
-        self.update()
+	def mousePressEvent(self, event):
+		self.begin = event.pos()
+		self.end = self.begin
+		self.update()
 
-    def mouseMoveEvent(self, event):
-        self.end = event.pos()
-        self.update()
+	def mouseMoveEvent(self, event):
+		self.end = event.pos()
+		self.update()
 
-    def mouseReleaseEvent(self, event):
-        self.close()
+	def mouseReleaseEvent(self, event):
+		self.close()
+
+	def get_captured_coords(self):
+		x1 = min(self.begin.x(), self.end.x())
+		y1 = min(self.begin.y(), self.end.y())
+		x2 = max(self.begin.x(), self.end.x())
+		y2 = max(self.begin.y(), self.end.y())
+		return x1, y1, x2, y2
 
 
-def capture():
-    app = QtWidgets.QApplication(sys.argv)
-    st = SnippingTool()
-    st.show()
-    # print(dir(app))
-    app.exec_()
-    # app.quit()
-
-    x1 = min(st.begin.x(), st.end.x())
-    y1 = min(st.begin.y(), st.end.y())
-    x2 = max(st.begin.x(), st.end.x())
-    y2 = max(st.begin.y(), st.end.y())
-
-    return x1, y1, x2, y2
+def main():
+	app = QtWidgets.QApplication(sys.argv)
+	st = SnippingTool()
+	st.show()
+	app.exec_()
+	app.quit()
+	print(st.get_captured_coords())
 
 
 if __name__ == '__main__':
-    print(capture())
+	main()
--- typing_game.py
+++ typing_game.py
@@ -9,88 +9,94 @@ import pyautogui as pyag
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import cv2
-import snipping_tool
+from snipping_tool import SnippingTool
+from PyQt5 import QtWidgets
+import sys
 
 
 def main():
+	# change current directory to the directory where this script file is
+	os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    # change current directory to the directory where this script file is
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+	# path of tesseract
+	tesseract_dir = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+	pytesseract.pytesseract.tesseract_cmd = tesseract_dir
 
-    # path of tesseract
-    tesseract_dir = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
-    pytesseract.pytesseract.tesseract_cmd = tesseract_dir
+	# url of the game website
+	url = 'https://10fastfingers.com/typing-test/english'
 
-    # url of the game website
-    url = 'https://10fastfingers.com/typing-test/english'
+	# open up Chrome and go to the game website
+	chrome_options = Options()
+	chrome_options.add_argument('--window-position=0,0')
+	chrome_options.add_argument('--window-size=1000,600')
+	driver = webdriver.Chrome(chrome_options=chrome_options)
+	driver.get(url)
 
-    # open up Chrome and go to the game website
-    chrome_options = Options()
-    chrome_options.add_argument('--window-position=0,0')
-    chrome_options.add_argument('--window-size=1000,600')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get(url)
+	# get the pixel coordinates of the word box area
+	app = QtWidgets.QApplication(sys.argv)
+	st = SnippingTool()
+	st.show()
+	app.exec_()
+	app.quit()
+	x1, y1, x2, y2 = st.get_captured_coords()
 
-    # get the pixel coordinates of the word box area
-    x1, y1, x2, y2 = snipping_tool.capture()
+	# create plot area to display captured image
+	fig, ax = plt.subplots()
+	mngr = plt.get_current_fig_manager()
+	mngr.window.setGeometry(10, 800, 1300, 250)
+	plt.pause(0.001)
 
-    # create plot area to display captured image
-    fig, ax = plt.subplots()
-    mngr = plt.get_current_fig_manager()
-    mngr.window.setGeometry(10, 800, 1300, 250)
-    plt.pause(0.001)
+	pyag.click(x1, y1)
+	pyag.press('tab')
 
-    pyag.click(x1, y1)
-    pyag.press('tab')
+	while True:  # infinite loop until the game ends
+		print('-----------------------------------------')
+		print('Extracted words')
+		print('-----------------------------------------')
 
-    while True:  # infinite loop until the game ends
-        print('-----------------------------------------')
-        print('Extracted words')
-        print('-----------------------------------------')
+		# captured image from the screen
+		img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
 
-        # captured image from the screen
-        img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+		# display the captured image in the plot area
+		ax.imshow(img)
+		plt.pause(0.001)
 
-        # display the captured image in the plot area
-        ax.imshow(img)
-        plt.pause(0.001)
+		# binarize the captured image to improve
+		# the accuracy of optical character recognition (OCR)
+		img = np.array(img)
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)[1]
+		img = Image.fromarray(img)
 
-        # binarize the captured image to improve
-        # the accuracy of optical character recognition (OCR)
-        img = np.array(img)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)[1]
-        img = Image.fromarray(img)
+		# extract Egnlish words from the binarized image
+		ocr_words = pytesseract.image_to_string(img).split(' ')
 
-        # extract Egnlish words from the binarized image
-        ocr_words = pytesseract.image_to_string(img).split(' ')
+		for words in ocr_words:
+			# the last word in each row is connected to
+			# the first word in the next row with '\n' or '\n\n'
+			# that's why I split words here or it will cause errors
+			if '\n\n' in words:
+				words = words.split('\n\n')
+			elif '\n' in words:
+				words = words.split('\n')
+			else:
+				words = [words]
 
-        for words in ocr_words:
-            # the last word in each row is connected to
-            # the first word in the next row with '\n' or '\n\n'
-            # that's why I split words here or it will cause errors
-            if '\n\n' in words:
-                words = words.split('\n\n')
-            elif '\n' in words:
-                words = words.split('\n')
-            else:
-                words = [words]
+			for word in words:
+				try:
+					print(word)
+				except Exception as e:
+					print('unicode error')
+				# type word using puautogui
+				pyag.typewrite(word.replace(' ', '') + ' ')
+				time.sleep(0.2)
 
-            for word in words:
-                try:
-                    print(word)
-                except Exception as e:
-                    print('unicode error')
-                # type word using puautogui
-                pyag.typewrite(word.replace(' ', '') + ' ')
-                time.sleep(0.2)
-
-        if len(ocr_words) < 10:
-            print('Done!')
-            time.sleep(3)
-            driver.quit()
-            break
+		if len(ocr_words) < 10:
+			print('Done!')
+			time.sleep(3)
+			driver.quit()
+			break
 
 
 if __name__ == '__main__':
-    main()
+	main()
